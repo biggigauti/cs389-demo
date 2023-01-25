a0ab44bd5911dce8dee30b1502bb07b433e1c54e
Birgir Gauti <birgirgauti@gmail.com>
2023-01-25 09:29:30 -0700

    Modified build.gradle, index, helloController, and helloControllerTest.

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -1,12 +1,15 @@
 package edu.carroll.cs389;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
-        return "Hello from Spring Boot!";
+    @GetMapping("/")
+    public String index(@RequestParam(value="name", defaultValue="Student")String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
     }
 }
