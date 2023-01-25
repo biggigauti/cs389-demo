7a8c83389fc0c75fe8e8753f436905a6f436c3ab
Birgir Gauti <birgirgauti@gmail.com>
2023-01-25 15:13:26 -0700

    Fixed code. Checkpoint. Form Validation.

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String index(@RequestParam(value="name", required=false, defaultValue="Student")String name, Model model) {
         model.addAttribute("name", name);
         return "index";
